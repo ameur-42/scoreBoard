9ee7a98ee97eb399a82a60858cad693e911e9311
ameur-42 <oussamaameur42@gmail.com>
2024-06-30 11:42:32 +0100

    stopwatch

--- score.js
+++ score.js
@@ -1,6 +1,10 @@
 
 let scoreHome = 0
 let scoreGuest = 0
+let timer 
+let minute = 0; 
+let second = 0; 
+let count = 0;
 function addOne() {
     scoreHome += 1
     document.getElementById("score-home").textContent = scoreHome
@@ -39,4 +43,37 @@ function endGame() {
     scoreGuest = 0
     document.getElementById("score-home").textContent = scoreHome
     document.getElementById("score-guest").textContent = scoreGuest
+    timer = false
+    minute = 0
+    second = 0
+}
+function startGame() {
+    timer = true ;
+    stopWatch() ;
+}
+function stopGame() {
+    timer = false ;
+}
+
+function stopWatch() { 
+    if (timer) { 
+        count++; 
+  
+        if (count == 100) { 
+            second++; 
+            count = 0; 
+        } 
+  
+        if (second == 60) { 
+            minute++; 
+            second = 0; 
+        } 
+  
+        if (minute == 100) {  
+            minute = 0; 
+            second = 0; 
+        } 
+    }
+    document.getElementById("demo").textContent = minute + ":" + second
+    setTimeout(stopWatch, 10);
 }
